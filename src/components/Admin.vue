387d65fd9431ccf2bbc6bd9cff9ca8f9a9276357
aditya gupta <adityasgupta309@gmail.com>
2020-08-10 17:26:35 +0530

    vue auth route using client-server side

---
+++ src/components/Admin.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="hello">
+        <router-link to='/logout'>Log out</router-link>
+        <h1>Welcome to administrator page</h1>
+        <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'The superheros'
+    }
+  }
+}
+</script>
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+        text-decoration: none;
+    }
+    a {
+        color: #;
+        text-decoration: none;
+    }
+</style>
